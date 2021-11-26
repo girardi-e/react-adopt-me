ad8b8c6ae1ab2785e8c09922347bb738b7d8a618
emilio girardi <emilio.g.girardi@gmail.com>
2021-11-26 08:18:47 +0100

    Added comment

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,39 +1,40 @@
 //This is a custom hook which will be called inside Search Params
+//This is a basic comment
 
 import { useState, useEffect } from "react";
 
 const localCache = {};
 
 export default function useBreedList(animal) {
-    const [breedList, setBreedList] = useState([]);
+  const [breedList, setBreedList] = useState([]);
 
-    //show what the state of the hook is at
-    const [status, setStatus] = useState("unloaded");
+  //show what the state of the hook is at
+  const [status, setStatus] = useState("unloaded");
 
-    useEffect(() => {
-        if (!animal) {
-            setBreedList([]);
-        } else if (localCache[animal]) {
-            setBreedList(localCache[animal]);
-        } else {
-            requestBreedList();
-        }
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
 
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus("loading");
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
 
-            const res = await fetch(
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            );
-            const json = await res.json();
-            //return empty array if json is empty
-            localCache[animal] = json.breeds || [];
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      //return empty array if json is empty
+      localCache[animal] = json.breeds || [];
 
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
-        }
-    }, [animal]);
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
 
-    return [breedList, status];
+  return [breedList, status];
 }
