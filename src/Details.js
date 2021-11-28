723fa6ad88038cbd61efdaec546959c64808407e
emilio girardi <emilio.g.girardi@gmail.com>
2021-11-28 13:45:07 +0100

    Class component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,55 @@
-const Details = () => {
-  return <h2>Hi from details</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+//Class component
+class Details extends Component {
+  //working with state
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+  //rendering the component for the first time: This is similar to useEffect
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState(
+      //Passing the date retrieved into an object
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+// ANOTHER WAY TO DO IT
+//  this.setState({
+//    loading: false,
+//    name: json.pets[0].name,
+//    breed: json.pets[0].breed,
+//    animal: json.pets[0].animal,
+//  })
+
+  render() {
+    //destruct object
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{animal} - {breed} {city}, {state}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+//get props from router
+export default withRouter(Details);
--- src/Pet.js
+++ src/Pet.js
@@ -21,3 +21,25 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
 };
 
 export default Pet;
+
+
+
+//BEFORE
+//const Pet = () => {
+//  return React.createElement("div", {}, [
+//    React.createElement("h1", {}, "Luna"),
+//    React.createElement("h2", {}, "Dog"),
+//    React.createElement("h2", {}, "Havanese"),
+//  ]);
+//};
+//
+//const App = () => {
+//  return React.createElement("div", {}, [
+//    React.createElement("h1", {}, "Adopt Me!"),
+//    React.createElement(Pet),
+//    React.createElement(Pet),
+//    React.createElement(Pet),
+//  ]);
+//};
+//
+//ReactDOM.render(React.createElement(App), document.getElementById("root"));
